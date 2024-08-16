77e68e9637a23d1cffa843a67793fa677868dc90
SubratSethy <151069317+SubratSethy@users.noreply.github.com>
2024-08-16 18:03:30 +0530

    Add files via upload

---
+++ AppUseStateArray.js
@@ -0,0 +1,12 @@
+import React ,{UseState} from 'react';
+import './App.css';
+//import '/RulesHook.js';
+import '/UseStateArray.js';
+const App = () => {
+
+return (
+<div>
+  <RulesHook/>
+</div>
+)
+}
