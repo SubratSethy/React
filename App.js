7b6b62b473d1df2ac4041c64d6fe8fa911e82924
SubratSethy <151069317+SubratSethy@users.noreply.github.com>
2024-08-17 15:57:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React ,{UseState} from 'react';
 import './App.css';
 //import '/RulesHook.js';
-import '/UseStateArray.js';
+//import '/UseStateArray.js';
+import '/UseStateobject.js';
 const App = () => {
 
 return (
 <div>
-  <RulesHook/>
+  //<RulesHook/>
+  <UseStateobject>
 </div>
 )
 }
+export default App ;
